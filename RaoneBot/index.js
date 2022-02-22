87a273b5a1ffe370f5090616f1747153a89a1385
Test2022s <99396949+Test2022s@users.noreply.github.com>
2022-02-22 16:42:59 +0530

    Add files via upload

---
+++ RaoneBot/index.js
@@ -0,0 +1,10 @@
+//https://github.com/Test2022s/SL-ALFHA
+
+module.exports = {
+    Base: require('./Base'),
+    Message: require('./Message'),
+    StringSession: require('./StringSession'),
+    ReplyMessage: require('./ReplyMessage'),
+    Image: require('./Image'),
+    Video: require('./Video')
+};
