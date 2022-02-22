44dde7f2d00cb3a9dc794a9f4cef81eb9de9abcb
Test2022s <99396949+Test2022s@users.noreply.github.com>
2022-02-22 16:20:55 +0530

    Create Base.js

---
+++ RaoneBot/Base.js
@@ -0,0 +1,17 @@
+//https://github.com/Test2022s/SL-ALFHA
+
+'use strict';
+
+class Base {
+    constructor(client) {
+        Object.defineProperty(this, 'client', { value: client });
+    }
+
+    _clone() {
+        return Object.assign(Object.create(this), this);
+    }
+    
+    _patch(data) { return data; }
+}
+
+module.exports = Base;
